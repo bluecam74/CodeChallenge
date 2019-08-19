63566b869fde415659e285d07cd82945fc096834
bluecam74 <mabdulha@uci.edu>
2019-08-19 11:22:14 -0700

    Part 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter two numbers seperated by a comma:  ");
+            string userInput = Console.ReadLine();
+            string[] nums = userInput.Split(',');
+            int[] terms = new int[4];
+
+            for (int i=0; i<2; i++)
+            {
+                try {
+                    int result = Int32.Parse(nums[i]);
+                    terms[i]=result;
+                }
+                catch 
+                {
+                    int result = 0;
+                    terms[i]=result;
+                }
+            }
+            int sum = terms[0] + terms[1];
+            Console.WriteLine(sum);
+
+        }
+    }
+}
